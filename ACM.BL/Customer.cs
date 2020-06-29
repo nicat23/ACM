411eed7f8eef81c3e680bb4832c88477ebcacf73
Justin P <52590296+nicat23@users.noreply.github.com>
2020-06-29 00:16:40 -0500

    initial

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -8,5 +8,16 @@ namespace ACM.BL
 {
     public class Customer
     {
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
     }
 }
